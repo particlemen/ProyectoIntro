57fdd64319a5e05edf727566d894486aa35439de
alexmaximo06 <alex_maximo_06@hotmail.com>
2015-11-21 02:40:12 -0300

    Agregado codigo e info de Java para un primer programa

---
+++ Java/HolaMundo.java
@@ -0,0 +1,12 @@
+/**
+* La clase HolaMundo implementa una aplicación
+* que despliega por la salida estándar “Hola Mundo”
+*/
+public class HolaMundo
+{
+	public static void main(String[] args)
+	{
+		// Desplegar "Hola Mundo!"
+		System.out.println("Hola mgsundo");
+	}
+}
